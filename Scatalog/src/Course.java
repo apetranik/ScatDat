588e4f377798826083149ce1a0a576b23f7f3f8e
Apurva Gandhi <apurvaga@usc.edu>
2017-11-11 17:52:40 -0800

    Finished course except for SQL

--- Scatalog/src/Course.java
+++ Scatalog/src/Course.java
@@ -20,7 +20,104 @@ public class Course {
 		long workload = 0; 
 		for(Review review : reviews) {
 			Score score = review.getScore(); 
-			enjoyment += score.get
+			enjoyment += score.getEnjoyment(); 
+			difficulty += score.getDifficulty(); 
+			value += score.getValue(); 
+			workload += score.getWorkload(); 
 		}
+		overallScore.setWorkload(workload/((double) reviews.size()));
+		overallScore.setEnjoyment(enjoyment/((double) reviews.size()));
+		overallScore.setValue(value/((double) reviews.size()));
+		overallScore.setDifficulty(difficulty/((double) reviews.size()));
+		ScoreMap calculator = new ScoreMap(); 
+		overallScore.setOverallRating(calculator.computeOverallScore(overallScore)); 
 	}
+	
+	public addReview(Review review) {
+		reviews.add(review); 
+		
+		//// ADD TO DATABASE ////
+	}
+
+	public String getCourseName() {
+		return courseName;
+	}
+
+	public void setCourseName(String courseName) {
+		this.courseName = courseName;
+	}
+
+	public int getCourseId() {
+		return courseId;
+	}
+
+	public void setCourseId(int courseId) {
+		this.courseId = courseId;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public int getNumRatings() {
+		return numRatings;
+	}
+
+	public void setNumRatings(int numRatings) {
+		this.numRatings = numRatings;
+	}
+
+	public String getPrefix() {
+		return prefix;
+	}
+
+	public void setPrefix(String prefix) {
+		this.prefix = prefix;
+	}
+
+	public Score getOverallScore() {
+		return overallScore;
+	}
+
+	public void setOverallScore(Score overallScore) {
+		this.overallScore = overallScore;
+	}
+
+	public HashMap<Name, ProfCourse> getProfCourses() {
+		return profCourses;
+	}
+
+	public void setProfCourses(HashMap<Name, ProfCourse> profCourses) {
+		this.profCourses = profCourses;
+	}
+
+	public ArrayList<Review> getReviews() {
+		return reviews;
+	}
+
+	public void setReviews(ArrayList<Review> reviews) {
+		this.reviews = reviews;
+	}
+
+	public int getType() {
+		return type;
+	}
+
+	public void setType(int type) {
+		this.type = type;
+	}
+
+	public int getNumUsers() {
+		return numUsers;
+	}
+
+	public void setNumUsers(int numUsers) {
+		this.numUsers = numUsers;
+	}
+	
+	
 }
