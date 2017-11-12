5eceb923c66657c4b29193089b2a18f1e123db10
Krishna Gopal <gopalk@usc.edu>
2017-11-12 00:29:03 -0800

    Adding query stuff

--- Scatalog/src/database/Database.java
+++ Scatalog/src/database/Database.java
@@ -10,18 +10,22 @@ import java.util.Map;
 import java.util.Set;
 import java.util.concurrent.ConcurrentSkipListSet;
 
+import com.mysql.jdbc.Driver;
+import com.mysql.jdbc.PreparedStatement;
+
 import scatalogObjects.Course;
 import tools.ScoreMap;
 
+
 public class Database {
-	Connection conn = null;
-	Statement st = null;
-	ResultSet rs = null;
+	private Connection conn = null;
+	private Statement st = null;
+	private ResultSet rs = null;
 	
 	public Database() {
 		try {
 			Class.forName("com.mysql.jdbc.Driver");
-			conn = DriverManager.getConnection("jdbc:mysql://localhost/studentgrades?user=root&password=root&useSSL=false");
+			conn = DriverManager.getConnection("jdbc:mysql://localhost/scatdat?user=root&password=root&useSSL=false");
 			st = conn.createStatement();
 		}catch(SQLException sqle) {
 			System.out.println (sqle.getMessage());
@@ -31,63 +35,89 @@ public class Database {
 	}
 	
 	public String queryPassword(String username) {
-		
-	}
-	
-	public String queryClassStanding(String username) {
-		
-	}
-	
-	public List<Course> queryCourseTaken(String username) {
-		// get the list of classID that this student has taken from the CourseTaken table
+		String name = username;
+		// rs = st.executeQuery("SELECT * from Student where fname='" + name + "'");
+		PreparedStatement ps = null; 
+		String fullName = "";
+		try {
+			ps = (PreparedStatement) conn.prepareStatement("SELECT * FROM user WHERE username=?");
+			ps.setString(1, name);
+			rs = ps.executeQuery();
 			
-		// instantiate each class using queryCourse(int classID)
-		
-		// return the list of Course
-	}
-	
-	public ScoreMap queryScoreMap(String username) {
-		// get the 20 scores of the user
-		
-		// instantiate with ScoreMap with its constructor;
-	}
-	
-	public Course queryCourse(int classID) {
-		// call 
-		
-		// call score constructor
-	}
-	
-	public String queryCourseName(int classID) {
+			while (rs.next()) {
+				fullName += rs.getString("fname");
+				fullName += " ";
+				fullName += rs.getString("lname");
+			}
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} // set first variable in prepared statement
+		return fullName;
 		
 	}
-	
-	public String queryCourseDescription(int classID) {
-		
+	public static void main(String [] args) {
+		Database db = new Database();
+		String name = db.queryPassword("gopalk");
+		System.out.println(name);
+		String otherName = db.queryPassword("petranik");
+		System.out.println(otherName);
 	}
 	
-	public int queryNumberOfRatings(int classID) {
-		
-	}
-	
-	public int queryPrefix(int classID) {
-		
-	}
-	
-	public double queryOverallscore(int classID) {
-		
-	}
-	
-	public List<Review> queryReviews(int classID){
-		
-	}
-	
-	public int queryType(int classID) {
-		
-	}
-	
-	public Map<Name, ProfCourse> queryProfCourse(){
-		
-	}
-	
+//	public String queryClassStanding(String username) {
+//		
+//	}
+//	
+//	public List<Course> queryCourseTaken(String username) {
+//		// get the list of classID that this student has taken from the CourseTaken table
+//			
+//		// instantiate each class using queryCourse(int classID)
+//		
+//		// return the list of Course
+//	}
+//	
+//	public ScoreMap queryScoreMap(String username) {
+//		// get the 20 scores of the user
+//		
+//		// instantiate with ScoreMap with its constructor;
+//	}
+//	
+//	public Course queryCourse(int classID) {
+//		// call 
+//		
+//		// call score constructor
+//	}
+//	
+//	public String queryCourseName(int classID) {
+//		
+//	}
+//	
+//	public String queryCourseDescription(int classID) {
+//		
+//	}
+//	
+//	public int queryNumberOfRatings(int classID) {
+//		
+//	}
+//	
+//	public int queryPrefix(int classID) {
+//		
+//	}
+//	
+//	public double queryOverallscore(int classID) {
+//		
+//	}
+//	
+//	public List<Review> queryReviews(int classID){
+//		
+//	}
+//	
+//	public int queryType(int classID) {
+//		
+//	}
+//	
+//	public Map<Name, ProfCourse> queryProfCourse(){
+//		
+//	}
+//	
 }
