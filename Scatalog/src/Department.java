533a30f4d9fa42b2bbfdff0989f3c40880596b47
Krishna Gopal <gopalk@usc.edu>
2017-11-11 19:11:59 -0800

    Add department

--- Scatalog/src/Department.java
+++ Scatalog/src/Department.java
@@ -1,4 +1,27 @@
+import java.util.Vector;
 
 public class Department {
-
+	private String prefix;
+	private Vector<Course> courses;
+	public Department(String prefix) {
+		this.prefix = prefix;
+		// DATABASE
+	}
+	public void addCourse(Course course) {
+		this.courses.add(course);
+		// DATABASE
+	}
+	public String getPrefix() {
+		return prefix;
+	}
+	public void setPrefix(String prefix) {
+		this.prefix = prefix;
+	}
+	public Vector<Course> getCourses() {
+		return courses;
+	}
+	public void setCourses(Vector<Course> courses) {
+		this.courses = courses;
+	}
+	
 }
