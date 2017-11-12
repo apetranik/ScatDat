9b463bacdf09ae3da32852e6c80dc3d0deb7255e
Qiusi Li <qiusili@usc.edu>
2017-11-11 18:42:52 -0800

    qiusi_6:42

--- Scatalog/src/database/Database.java
+++ Scatalog/src/database/Database.java
@@ -1,10 +1,48 @@
 package database;
 
-import org.apache.tomcat.util.buf.StringCache;
+import java.util.List;
+import java.util.Set;
+import java.util.concurrent.ConcurrentSkipListSet;
 
-public class QueryUser {
+public class Database {
 	
-	public String getPassword() {
+	public String queryPassword(String username) {
+		
+	}
+	
+	public String queryClassStanding(String username) {
+		
+	}
+	
+	public List<Course> queryCourseTaken(String username) {
+		// get the list of classID that this student has taken from the CourseTaken table
+			
+		// instantiate each class using queryCourse(int classID)
+		
+		// return the list of Course
+	}
+	
+	public ScoreMap queryScoreMap(String username) {
+		// get the 20 scores of the user
+		
+		// instantiate with ScoreMap with its constructor;
+	}
+	
+	public Course queryCourse(int classID) {
+		// call 
+		
+		// call score constructor
+	}
+	
+	public String queryCourseName(int classID) {
+		
+	}
+	
+	public String queryCourseDescription(int classID) {
+		
+	}
+	
+	public int queryNumberOfRatings(int classID) {
 		
 	}
 }
